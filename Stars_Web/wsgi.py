e66c2759614d8257c27db4e2f37422dea82948e0
ylnx <799613500@qq.com>
2020-09-30 11:38:59 +0800

    项目初始化

---
+++ Stars_Web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Stars_Web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stars_Web.settings')
+
+application = get_wsgi_application()
